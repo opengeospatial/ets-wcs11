07d61ee12402e01ed7f0b777c188253ab34443f9
Keshav Nangare <keshav.nangare@mindstix.com>
2019-09-06 16:40:17 +0530

    Invoke test suite using REST API. #50

---
+++ src/main/java/org/opengis/cite/wcs11/WCS11.java
@@ -0,0 +1,17 @@
+package org.opengis.cite.wcs11;
+
+/**
+ * Contains various constants defined in the WMS 1.3 specification, such as test
+ * run parameters.
+ * 
+ * @see "OGC 06-042: OpenGIS Web Map Server Implementation Specification, Version 1.3.0"
+ */
+public class WCS11 {
+
+    private WCS11() {
+    }
+
+    /** Supplies reference to wcs capabilities document (URI). */
+    public static final String URL = "url";
+
+}
