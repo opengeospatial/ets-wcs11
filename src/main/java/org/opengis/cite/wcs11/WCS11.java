157e3a0e4704321d2f57d80ab0bf57fb45da41dd
Benjamin Proß <b.pross@52north.org>
2025-01-22 09:10:04 +0100

    Adjust java code

--- src/main/java/org/opengis/cite/wcs11/WCS11.java
+++ src/main/java/org/opengis/cite/wcs11/WCS11.java
@@ -1,17 +1,17 @@
 package org.opengis.cite.wcs11;
 
 /**
- * Contains various constants defined in the WCS 1.1.1 specification, such as test
- * run parameters.
- * 
+ * Contains various constants defined in the WCS 1.1.1 specification, such as test run
+ * parameters.
+ *
  * @see "OGC 07-067r2: OGC Web Coverage Service (WCS) Implementation Specification"
  */
 public class WCS11 {
 
-    private WCS11() {
-    }
+	private WCS11() {
+	}
 
-    /** Supplies reference to wcs capabilities document (URI). */
-    public static final String URL = "url";
+	/** Supplies reference to wcs capabilities document (URI). */
+	public static final String URL = "url";
 
 }
